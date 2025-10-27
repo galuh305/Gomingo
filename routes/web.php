a50a68206c171472c1b2b94b2e3c899197b8692d
Zara Ayunda <zaraayunda1402@gmail.com>
2025-10-27 15:39:44 +0700

    Pembuatan Template Awal

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,14 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layout/main');
+});
+Route::get('/dashboard', function () {
+    return view('layout/dashboard');
+});
+Route::get('/login', function () {
+    return view('login/v_login');
+});
+Route::get('/registrasi', function () {
+    return view('login/v_registrasi');
 });
